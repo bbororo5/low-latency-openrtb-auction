291dbbfbfa16347cf1c156944eac95df4530caab
bbororo5 <chdusly3@gmail.com>
2026-07-02 23:07:15 +0900

    refactor: align impression model with openrtb media objects

--- shared/src/main/java/com/bbororo/rtb/shared/openrtb/Imp.java
+++ shared/src/main/java/com/bbororo/rtb/shared/openrtb/Imp.java
@@ -1,12 +1,12 @@
 package com.bbororo.rtb.shared.openrtb;
 
-import com.bbororo.rtb.shared.common.MediaType;
-
 import java.math.BigDecimal;
 
 public record Imp(
         String id,
-        MediaType mediaType,
+        Banner banner,
+        Video video,
+        NativeAd nativeAd,
         BigDecimal bidfloor,
         String bidfloorcur
 ) {
